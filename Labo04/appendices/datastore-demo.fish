6cf7c008d938226bbe92330cc3b7d6036dfefa28
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2024-04-25 14:15:34 +0200

    rfr: accept base url as argument in demo script

--- Labo04/appendices/datastore-demo.fish
+++ Labo04/appendices/datastore-demo.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+# Take the base url as parameter
+set base_url $argv[1]
+
 set dict /usr/share/dict/words
 set num_words (wc -l < $dict)
 
@@ -10,8 +13,8 @@ for i in (seq 1 10)
         set random_word_author (shuf -n 1 $dict)
         set encoded_word_author (echo $random_word_author | sed 's/ /%20/g')
         if test $i -lt 10
-                http "http://localhost:8080/datastorewrite?_kind=book&author=$encoded_word_author&title=$encoded_word_title" > /dev/null
+                http "$base_url/datastorewrite?_kind=book&author=$encoded_word_author&title=$encoded_word_title" > /dev/null
             else
-                http "http://localhost:8080/datastorewrite?_kind=book&author=$encoded_word_author&title=$encoded_word_title"
+                http "$base_url/datastorewrite?_kind=book&author=$encoded_word_author&title=$encoded_word_title"
             end
 end
